4881fed57ed8418d5a0525c7883022756d1826c1
saumyaborwankar <thesaumyaborwankar@gmail.com>
2023-07-01 13:56:02 -0500

    pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Homepage from "./components/homepage/Homepage";
+import { Route, Router, Routes } from "react-router-dom";
+import Login from "./components/login/Login";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
+      {/* <Routes> */}
       <Navbar />
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Homepage />} /> */}
+      {/* <Route path="/create" element={<Create />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/message/:id" element={<SendMessage />} /> */}
+      {/* <Route path="/aboutme" element={<AboutMe />} /> */}
+      {/* <Route path="/disclaimer" element={<Disclaimer />} /> */}
+      {/* </Routes> */}
+      <Footer />
+      {/* </Routes> */}
     </div>
   );
 }
---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,7 @@
+import "./Homepage.css";
+
+function Homepage() {
+  return <div>Homepage</div>;
+}
+
+export default Homepage;
